da450df459bb316e2370c27e2343052570866436
mushfiqur <mushfiqur15-10872@diu.edu.bd>
2021-09-10 22:26:19 +0600

    Splash added

---
+++ app/src/main/java/com/example/carelesscoders/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.carelesscoders;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
